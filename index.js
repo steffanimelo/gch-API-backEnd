abb2ebb1cd9bab6a5102d0fb8a0c5358e5830a16
Steffani <sartinisteffani@gmail.com>
2022-04-22 16:36:33 +0200

    database testing as an array

--- index.js
+++ index.js
@@ -9,14 +9,4 @@ app.get('/', (req, res) => res.send('Game Characters API'));
 
 app.listen(port,() => console.log(`Server running at http://localhost:${port}`));
 
-/*const { resolve } = require('path');   // resolving the absolute path for langingPage
-app.get('/characters', (req, res) => {
-    const myTitle = 'Game Characterts API - Final Fantasy';
-    const characters = [
-    {id:1,name:'Thorr', origin: 'Pole', age: '400', weakness: 'fire', strength: 'ice', Weapon: 'Hammer', game: 'FF4', Description: 'Some text wwwwwwwwwwwwwwwwwwwwwwwwwwwww'},
-    {id:2,name:'Frea', origin: 'Sun', age: '1000', weakness: 'unknown', strength: 'fire', Weapon: 'Mind', game: 'FF8', Description: 'Some text wwwwwwwwwwwwwwwwwwwwwwwwwwwww'},
-    {id:3,name:'BigFoot', origin: 'Mars', age: '300', weakness: 'eye', strength: 'Dust', Weapon: 'Gun', game: 'FF2', Description: 'Some text wwwwwwwwwwwwwwwwwwwwwwwwwwwww'},
-]
-});
-*/
 
--- routes/playgroundRouter.js
+++ routes/playgroundRouter.js
@@ -2,11 +2,16 @@ import { Router } from "express";
 
 const playgroundRouter = Router();
 
+//database testing as an array   
+const characters = [
+    {id:1,name:'Thorr', origin: 'Pole', age: '400', weakness: 'fire', strength: 'ice', Weapon: 'Hammer', game: 'FF4', Description: 'Some text wwwwwwwwwwwwwwwwwwwwwwwwwwwww'},
+    {id:2,name:'Frea', origin: 'Sun', age: '1000', weakness: 'unknown', strength: 'fire', Weapon: 'Mind', game: 'FF8', Description: 'Some text wwwwwwwwwwwwwwwwwwwwwwwwwwwww'},
+    {id:3,name:'BigFoot', origin: 'Mars', age: '300', weakness: 'eye', strength: 'Dust', Weapon: 'Gun', game: 'FF2', Description: 'Some text wwwwwwwwwwwwwwwwwwwwwwwwwwwww'},
+];
+
 playgroundRouter  
    .route('/')
-   .get((req, res) => {
-      res.send('Get all Characters');
-  })
+   .get((req, res) => res.json(characters))
   .post((req, res) => res.send('Add new Character'));
 
 playgroundRouter
