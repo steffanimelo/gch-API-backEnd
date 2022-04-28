fa0f466e6e433ddcba120582372514eefd3de293
Steffani <sartinisteffani@gmail.com>
2022-04-28 15:21:15 +0200

    Simple error handers for requests

--- controllers/characters.js
+++ controllers/characters.js
@@ -9,7 +9,14 @@ export const getAllCharacters = asyncHandler (async (req, res, next) => {
     res.json(posts);
 });
 
-export const getSingleCharacter = asyncHandler(async (req, res) => res.send('Getting a Single Character'));
+export const getSingleCharacter = asyncHandler(async (req, res) => {
+    const {
+        params: {id}            // how can I get a single with Index or keyword?
+    } = req;
+    const post = await Post.findById(id);
+    if(!post) throw new ErrorResponse(`Charater with this ${id} doesn't exist!`, 404);  // Joi will substitute this one
+    res.json(post);
+});
 
 export const createCharacter = asyncHandler(async (req, res) => {
     const {body} = req;
@@ -17,6 +24,22 @@ export const createCharacter = asyncHandler(async (req, res) => {
     res.status(201).json(newPost);
 });
 
-export const updateCharacter = asyncHandler (async (req, res) => res.send('You just changed an existing Character!'));
+export const updateCharacter = asyncHandler (async (req, res) => {
+    const {
+        body,
+        params: { id }
+    } = req;
+    const updateCharacter = await Post.findOneAndUpdate({ _id: id }, body, { new: true });
+    if(!updateCharacter) throw new ErrorResponse(`Charater with this ${id} doesn't exist!`, 404);  // Joi will substitute this one
+    res.json(updateCharacter);
+});
 
-export const deleteCharacter = asyncHandler (async (req, res) => res.send('You just deleted a Character!'));
+
+export const deleteCharacter = asyncHandler (async (req, res) => {
+    const {
+        params: { id }
+    } = req;
+    const deleted = await Post.findByIdAndDelete(id);
+    if(!deleted) throw new ErrorResponse(`Charater with this ${id} doesn't exist!`, 404);  // Joi will substitute this one
+    res.json({success: `Charater with this ${id} was deleted!`})
+});
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,10 @@
 const errorHandler = (err, req, res, next) => {
     if (process.env.NODE_ENV !== 'development') {
-    console.error(err.stack);  
+    console.log(err.name);
+    console.log(err.message);   
+    }
+    if(err.name === 'CastError'){
+        return res.status(400).json({error:'What you passed is an invalid objectId!'});
     }
     res.status(err.statusCode || 500).json({ error: err.message});      
 };
