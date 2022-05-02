56609544ecd3388c46546c1a5cea8261f5072b4b
Steffani <sartinisteffani@gmail.com>
2022-05-02 19:51:26 +0200

    ajustments with error handling

--- utils/ErrorResponse.js
+++ utils/ErrorResponse.js
@@ -1,8 +1,9 @@
 class ErrorResponse extends Error {
-    constructor(message, statusCode = 500) {
-        super(message);
-        this.statusCode = statusCode;
-    }
+  constructor(message, statusCode = 500, name = "Character API error") {
+    super(message);
+    this.name = name;
+    this.statusCode = statusCode;
+  }
 }
 
 export default ErrorResponse;
