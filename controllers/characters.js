ea0423e492193301b39d327098908d1420a7df79
Steffani <sartinisteffani@gmail.com>
2022-05-02 11:06:07 +0200

    Cors installation - front and back-end connection within the Playground

--- controllers/characters.js
+++ controllers/characters.js
@@ -2,49 +2,50 @@ import ErrorResponse from "../utils/ErrorResponse.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 import Charater from "../models/Character.js";
 
+export const getAllCharacters = asyncHandler(async (req, res, next) => {
+  const characters = await Charater.find(req.query);
+  console.log(req.query);
 
-
-export const getAllCharacters = asyncHandler (async (req, res, next) => {
-    const characters = await Charater.find()
-    const {params: {name, id}} = req
-    await Charater.find({$or:[{name}, {id}]});                     //In need of testing in the front-End
-    res.json(characters);
+  //const {params: {name, id}} = req       //req.query is working fine =)
+  //await Charater.find({$or:[{name}, {id}]}); //In need of testing in the front-End
+  res.json(characters);
 });
-
 
 export const getSingleCharacter = asyncHandler(async (req, res) => {
-    const {
-        params: {id}           
-    } = req;                  
-    const character = await Charater.findById(id);
-    if(!character) throw new ErrorResponse(`Charater with this ${id} doesn't exist!`, 404);  // Joi will substitute this one
-    res.json(character);
+  const {
+    params: { id },
+  } = req;
+  const character = await Charater.findById(id);
+  if (!character)
+    throw new ErrorResponse(`Charater with this ${id} doesn't exist!`, 404); // Joi will substitute this one
+  res.json(character);
 });
-
 
 export const createCharacter = asyncHandler(async (req, res) => {
-    const {body} = req;
-    const newCharater = await Charater.create(body);
-    res.status(201).json(newCharater);
+  const { body } = req;
+  const newCharater = await Charater.create(body);
+  res.status(201).json(newCharater);
 });
 
-export const updateCharacter = asyncHandler (async (req, res) => {
-    const {
-        body,
-        params: { id }
-    } = req;
-    const updateCharacter = await Charater.findOneAndUpdate({ _id: id }, body, { new: true });
-    if(!updateCharacter) throw new ErrorResponse(`Charater with this ${id} doesn't exist!`, 404);  // Joi will substitute this one
-    res.json(updateCharacter);
+export const updateCharacter = asyncHandler(async (req, res) => {
+  const {
+    body,
+    params: { id },
+  } = req;
+  const updateCharacter = await Charater.findOneAndUpdate({ _id: id }, body, {
+    new: true,
+  });
+  if (!updateCharacter)
+    throw new ErrorResponse(`Charater with this ${id} doesn't exist!`, 404); // Joi will substitute this one
+  res.json(updateCharacter);
 });
 
-
-export const deleteCharacter = asyncHandler (async (req, res) => {
-    const {
-        params: { id }
-    } = req;
-    const deleted = await Charater.findByIdAndDelete(id);
-    if(!deleted) throw new ErrorResponse(`Charater with this ${id} doesn't exist!`, 404);  // Joi will substitute this one
-    res.json({success: `Charater with this ${id} was deleted!`})
+export const deleteCharacter = asyncHandler(async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  const deleted = await Charater.findByIdAndDelete(id);
+  if (!deleted)
+    throw new ErrorResponse(`Charater with this ${id} doesn't exist!`, 404); // Joi will substitute this one
+  res.json({ success: `Charater with this ${id} was deleted!` });
 });
-
--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-
 import "./db/db-index.js";
 import express from "express";
 import cookieParser from "cookie-parser";
 import fs from "fs";
-import errorHandler from "./middlewares/errorHandler.js"
+import errorHandler from "./middlewares/errorHandler.js";
 import playgroundRouter from "./routes/characterRouter.js";
 import tracker from "./middlewares/tracker.js";
 import { nextTick } from "process";
+import cors from "cors";
 
-const app = express();     //function that serves as a middleware and returnes:  (req, res, next) => {} 
+const app = express(); //function that serves as a middleware and returnes:  (req, res, next) => {}
 const port = process.env.PORT || 5050;
 
 app.use(cookieParser());
+/*---------------  CORS ---------------------*/
+app.use(
+  cors()
+  //       {
+  //     origin: ["http://localhost:3000/playground"],
+  //     methods: ["GET"],
+  //   }
+);
 
-app.use(express.json());   // middleware that processes the incoming raw json (our Body inputs)
+/*---------------  CORS ---------------------*/
+app.use(express.json()); // middleware that processes the incoming raw json (our Body inputs)
 
 app.use(tracker);
-app.use('/playground', playgroundRouter);
+app.use("/playground", playgroundRouter);
 
-app.use(errorHandler);    // the order matters here; it has to be at the end
+app.use(errorHandler); // the order matters here; it has to be at the end
 
-app.listen(port,() => console.log(`Server running at http://localhost:${port}`));
-
-
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
